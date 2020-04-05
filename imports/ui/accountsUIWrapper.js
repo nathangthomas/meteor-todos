fad8a0839445be129cdb444d50af58bba5f1bdf7
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2020-04-05 14:10:11 -0600

    Create accounts UI wrapper component

---
+++ imports/ui/accountsUIWrapper.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { Template } from 'meteor/templating';
+import { Blaze } from 'meteor/blaze';
+
+// Have to wrap blaze UI component in React component in order to work since we are not using default Meteor Blaze UI.
+
+export default class AccountsUIWrapper extends Component {
+  componentDidMount() {
+    // Use Meteor Blaze to render login buttons
+    this.view = Blaze.render(Template.loginButtons,
+      ReactDOM.findDOMNode(this.refs.container));
+  }
+  componentWillUnmount() {
+    // Clean up Blaze view
+    Blaze.remove(this.view);
+  }
+  render() {
+    // Just render a placeholder container that will be filled in
+    return <span ref="container" />;
+  }
+}

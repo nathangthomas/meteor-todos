f74c6d2c217508dd48639c7b022bcab29337403c
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2020-04-05 12:56:10 -0600

    Configure for running app on mobile

--- server/main.js
+++ server/main.js
@@ -4,6 +4,15 @@ import '../imports/api/tasks.js';
 // ^^ creates the MongoDB collection and sets up the plumbing to get the data to the client
 
 
-Meteor.startup(() => {
-  // code to run on server at startup
+// Meteor.startup(() => {
+//   // code to run on server at startup
+//
+// });
+
+if (Meteor.isServer) {
+Meteor.startup(function () {
+// code to run on server at startup
+process.env.MOBILE_DDP_URL = ‘http://1.1.1.1:3000 1’;
+process.env.MOBILE_ROOT_URL = ‘http://1.1.1.1:3000 1’;
 });
+}

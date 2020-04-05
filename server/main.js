24f7d78ad669c4851ea26cad9be5173c56d333ed
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2020-04-04 22:51:20 -0600

    Import api/tasks.js

--- server/main.js
+++ server/main.js
@@ -1,4 +1,8 @@
 import { Meteor } from 'meteor/meteor';
+import '../imports/api/tasks.js';
+
+// ^^ creates the MongoDB collection and sets up the plumbing to get the data to the client
+
 
 Meteor.startup(() => {
   // code to run on server at startup

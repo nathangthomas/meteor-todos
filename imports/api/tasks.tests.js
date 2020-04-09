6eef2a2bc8eb9f16fb76a6721431e1e1cf96aeae
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2020-04-08 20:51:47 -0600

    Add closing curly brace and simicolon

--- imports/api/tasks.tests.js
+++ imports/api/tasks.tests.js
@@ -2,8 +2,11 @@
 
 import { Meteor } from 'meteor/meteor';
 import { Random } from 'meteor/random';
-import { Tasks } from './tasks.js';
 import { assert } from 'chai';
+
+import { Tasks } from './tasks.js';
+
+
 
 if (Meteor.isServer) {
   describe('Tasks', () => {
@@ -21,18 +24,31 @@ if (Meteor.isServer) {
         });
       });
 
-      it('can delete owned task', () => {
-        // Find the internal implementation of the task method so we can
-        // test it in isolation
-        const deleteTask = Meteor.server.method_handlers['tasks.remove'];
 
-        // Set up a fake method invocation that looks like what the method expects
-        const invocation = { userId };
-
-        // Run the method with `this` set to the fake invocation
-        deleteTask.apply(invocation, [taskId]);
-
-        // Verify that the method does what we expected
-        assert.equal(Tasks.find().count(), 0);
+      beforeEach(() => {
+        Tasks.remove({});
+        taskId = Tasks.insert({
+          text: 'test task',
+          createdAt: new Date(),
+          owner: userId,
+          username: 'tmeasday',
+        });
       });
-    });
+
+      it('can delete owned task', () => {
+         // Find the internal implementation of the task method so we can
+         // test it in isolation
+         const deleteTask = Meteor.server.method_handlers['tasks.remove'];
+
+         // Set up a fake method invocation that looks like what the method expects
+         const invocation = { userId };
+
+         // Run the method with `this` set to the fake invocation
+         deleteTask.apply(invocation, [taskId]);
+
+         // Verify that the method does what we expected
+         assert.equal(Tasks.find().count(), 0);
+       });
+     });
+   });
+};

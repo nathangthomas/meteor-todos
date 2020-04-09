87de87db628cb231576a66340a2c9a79be44a617
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2020-04-09 00:41:57 -0600

    Update server/main.js

--- server/main.js
+++ server/main.js
@@ -1,30 +1,16 @@
-// import { Meteor } from 'meteor/meteor';
-// import '../imports/api/tasks.js';
-// import '../imports/ui/AccountsUIWrapper.js'
-//
-// // ^^ creates the MongoDB collection and sets up the plumbing to get the data to the client
-//
-//
-// Meteor.startup(() => {
-//   // code to run on server at startup
-//
-// });
-//
-// // if (Meteor.isServer) {
-// // Meteor.startup(function () {
-// // // code to run on server at startup
-// // process.env.MOBILE_DDP_URL = "http://1.1.1.1:3000 1";
-// // process.env.MOBILE_ROOT_URL = "http://1.1.1.1:3000 1";
-// // });
-// // }
-
-
-import React from 'react';
 import { Meteor } from 'meteor/meteor';
-import { render } from 'react-dom';
-import '../imports/startup/accounts-config.js';
-import App from '../imports/ui/App.js';
+import '../imports/api/tasks.js';
+// ^^ creates the MongoDB collection and sets up the plumbing to get the data to the client
+
 
 Meteor.startup(() => {
-  render(<App />, document.getElementById('render-target'));
+  // code to run on server at startup
 });
+
+// if (Meteor.isServer) {
+// Meteor.startup(function () {
+// // code to run on server at startup
+// process.env.MOBILE_DDP_URL = "http://1.1.1.1:3000 1";
+// process.env.MOBILE_ROOT_URL = "http://1.1.1.1:3000 1";
+// });
+// }
